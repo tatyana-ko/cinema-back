caa1b4481ac42007c8992b383ba4daec6aead5e9
Tatyana Koba <misskotanyaz@gmail.com>
2025-05-12 15:48:40 +0300

    feat: add configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,45 @@
 // @ts-check
 import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import prettierPlugin from 'eslint-plugin-prettier';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  {
-    ignores: ['eslint.config.mjs'],
-  },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-      ecmaVersion: 5,
-      sourceType: 'module',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-  },
-  {
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
-    },
-  },
+	{
+		ignores: ['node_modules', 'dist', 'build', '*.config.js', '*config.mjs'],
+	},
+
+	eslint.configs.recommended,
+	...tseslint.configs.recommendedTypeChecked,
+	eslintPluginPrettierRecommended,
+
+	{
+		languageOptions: {
+			globals: {
+				...globals.node,
+				...globals.jest,
+			},
+			ecmaVersion: 'latest',
+			sourceType: 'module',
+			parserOptions: {
+				projectService: true,
+				tsconfigRootDir: import.meta.dirname,
+			},
+		},
+	},
+
+	{
+		plugins: {
+			prettier: prettierPlugin,
+		},
+
+		rules: {
+			'@typescript-eslint/no-explicit-any': 'warn',
+			'@typescript-eslint/no-floating-promises': 'error',
+			'@typescript-eslint/no-unsafe-argument': 'warn',
+			'@typescript-eslint/consistent-type-imports': 'warn',
+			'prettier/prettier': ['warn', { endOfLine: 'auto' }],
+		},
+	},
 );
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+	const app = await NestFactory.create(AppModule);
+	await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+
+bootstrap().catch(error => {
+	Logger.error('Bootstrap failed', (error as Error).stack || error);
+	process.exit(1);
+});
