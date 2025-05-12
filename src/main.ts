8ed5365af489729f2ab2084e63c2b35c6e46fcc2
Tatyana Koba <misskotanyaz@gmail.com>
2025-05-12 16:06:39 +0300

    feat: update main.ts(enable cors)

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,15 @@ import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	await app.listen(process.env.PORT ?? 3000);
+
+	app.setGlobalPrefix('api');
+	app.enableCors({
+		origin: ['http://localhost:3000'],
+		credentials: true,
+		exposedHeaders: 'set-cookie',
+	});
+
+	await app.listen(4200);
 }
 
 bootstrap().catch(error => {
