844fcaa727dd0f9b2af6748c4b5835e5ce8f012e
Tatyana Koba <misskotanyaz@gmail.com>
2025-05-25 12:22:40 +0300

    feat: add movie res

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { UserModule } from './user/user.module';
 import { GenreModule } from './genre/genre.module';
 import { FileModule } from './file/file.module';
 import { ActorModule } from './actor/actor.module';
+import { MovieModule } from './movie/movie.module';
 
 @Module({
-	imports: [AuthModule, UserModule, GenreModule, FileModule, ActorModule],
+	imports: [AuthModule, UserModule, GenreModule, FileModule, ActorModule, MovieModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
